808aca189b5a2b8a021969193ceb3b60d475489b
Brian Barr <snbbiz@gmail.com>
2020-11-01 19:14:20 -0500

    nearly done with confusion matrix for base class

--- 03_infer_qmnist.py
+++ 03_infer_qmnist.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import gzip
 import numpy as np
+from tensorflow.keras.models import load_model
+from sklearn.metrics import confusion_matrix as cm
 # based on 
 # https://gist.githubusercontent.com/ischlag/41d15424e7989b936c1609b53edd1390/raw/5ed7aca47bcca30b3df1c3bfd0f027e6bcdb430c/mnist-to-jpg.py
 
@@ -9,6 +11,11 @@ IMAGE_SIZE = 28
 NUM_CHANNELS = 1
 PIXEL_DEPTH = 255
 NUM_LABELS = 10
+
+# class specificaton
+base_class = 4
+target_class = 9
+
 
 def extract_data(filename, num_images):
   """Extract the images into a 4D tensor [image index, y, x, channels].
@@ -33,11 +40,29 @@ def extract_labels(filename, num_images):
     labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
   return labels
 
+def filter_base(x, y, base_class):
+    keep = np.where(y == base_class)[0]
+    y = y[keep]
+    x = x[keep, :]
+    print(keep.shape, y.shape, x.shape)
+
+    return x, y
+
 # images
 img_file = WORK_DIRECTORY + 'qmnist-test-images-idx3-ubyte.gz'
 qtest = extract_data(img_file, 60000)
 qtest /= 255
 
 label_file = WORK_DIRECTORY + 'qmnist-test-labels-idx1-ubyte.gz'
-qtest = extract_labels(label_file, 60000)
+qtest_labels = extract_labels(label_file, 60000)
 
+x_base_test, y_base_test = filter_base(qtest, qtest_labels, base_class)
+x_target_test, y_target_test = filter_base(qtest, qtest_labels, target_class)
+
+# load model
+classifier = load_model('models/a_classifier.h5', compile='False')
+
+base_pred = classifier.predict(x_base_test)
+y_base_pred = base_pred[:, 0] > 0.5
+
+base_cm = cm(y_base_test, y_base_pred)
